436eec41868d23ef5379189e62aeabeb0268135c
1Mill <erik.ekberg@outlook.com>
2019-04-20 17:52:43 -0700

    06

---
+++ go/src/06_conditionals/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x, y := 5, 10
+	// x, y := 15, 10
+	x, y := 10, 10
+
+	// If else
+	if x <= y {
+		fmt.Printf("%d is less than or equal to %d\n", x, y)
+	} else {
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
+
+	// Else if
+	// color := "red"
+	// color := "blue"
+	color := "green"
+
+	if color == "red" {
+		fmt.Println("Color is red")
+	} else if color == "blue" {
+		fmt.Println("Color is blue")
+	} else {
+		fmt.Println("Color is not red or blue")
+	}
+}
