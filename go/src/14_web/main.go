36b9033b0f041a04545de81ba882cf3500bb8fec
1Mill <erik.ekberg@outlook.com>
2019-04-21 14:34:18 -0700

    14 - Could not get server to expose from Docker container

---
+++ go/src/14_web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World!</h1>")
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>About!</h1>")
+}
+
+func main() {
+	// Like a router
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	fmt.Println("Server starting...")
+	http.ListenAndServe(":3000", nil)
+}
