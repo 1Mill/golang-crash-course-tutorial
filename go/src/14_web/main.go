c916a49b40648c17a5dfc6ab15c1d6090fd0a3a9
1Mill <erik.ekberg@outlook.com>
2019-04-22 20:02:16 -0700

    Try to overwrite local host base

--- go/src/14_web/main.go
+++ go/src/14_web/main.go
@@ -9,7 +9,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World!</h1>")
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>About!</h1>")
 }
 
@@ -18,5 +18,5 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
 	fmt.Println("Server starting...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("0.0.0.0:3000", nil)
 }
